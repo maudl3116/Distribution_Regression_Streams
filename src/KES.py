b24e26fc2c77ee48b7e81868f7db35d9c5a4a7ce
Maud LEMERCIER <ml3116@ic.ac.uk>
2021-06-29 21:00:55 +0100

    add sketch

--- src/KES.py
+++ src/KES.py
@@ -486,7 +486,7 @@ def model_higher_rank_sketch(X, y, depths1=[2], ncompos1=[20], rbf1=True, alphas
     # Precompute the Gram matrices for the different scaling parameters, to avoid recomputing them for each grid search step
     for (depth1,ncompo1, alpha1, lambda_, depth2, ncompo2, alpha2) in hyperparams:
 
-        pwCMKE = SketchpwCKMETransform(order=depth1, ncompo=ncompo1, rbf=rbf1, lengthscale=alpha1, lambda_=lambda_).fit_transform(X) 
+        pwCKME = SketchpwCKMETransform(order=depth1, ncompo=ncompo1, rbf=rbf1, lengthscale=alpha1, lambda_=lambda_).fit_transform(X) 
         ES = SketchExpectedSignatureTransform(order=depth2, ncompo=ncompo2, rbf=rbf2, lengthscale=alpha2).fit_transform(pwCKME)  #(M,D)
 
         mmd = -2*ES@ES.T
